01eedc2b57ca4ef562ddea6cc6bd25cd058239cc
Methamorphe <methamorphe@gmail.com>
2019-07-25 13:58:19 +0200

    Contact page + Responsive

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -5,7 +5,7 @@ import Header from 'src/components/Header';
 import Research from 'src/components/Research';
 import Footer from 'src/components/Footer';
 import Register from 'src/components/Register';
-import Connection from 'src/components/Connection';
+import Contact from 'src/components/Contact';
 
 import './app.scss';
 
@@ -14,7 +14,7 @@ const App = () => (
     <Header />
     <Switch>
       <Route path="/" exact component={Research} />
-      <Route path="/connection" component={Connection} />
+      <Route path="/contact" component={Contact} />
       <Route path="/inscription" component={Register} />
     </Switch>
     <Footer />
--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { FaFacebookSquare, FaTwitterSquare, FaGithubSquare } from 'react-icons/fa';
 
 import './footer.scss';
@@ -6,7 +7,7 @@ import './footer.scss';
 const Footer = () => (
   <footer className="footer">
     <ul className="footer-ul">
-      <li className="footer-items"><a href="#">Contact</a></li>
+      <li className="footer-items"><NavLink to="/contact">Contact</NavLink></li>
       <li className="footer-items"><a href="#">Mentions légales</a></li>
       <li className="footer-items"><a href="#">Qui sommes nous ?</a></li>
       <li id="icons" className="footer-items"><FaFacebookSquare size="1.5em" /> <FaTwitterSquare size="1.5em" /> <FaGithubSquare size="1.5em" /> </li>
