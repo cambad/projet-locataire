5188f087f3ba0f9649f3078e6cb08b1a156f9d3e
alexClmls <acalmels92@hotmail.fr>
2019-07-26 13:29:17 +0200

    CRUD entities except  User edit and User form

--- BackOffice_RateMyRent/src/Controller/Admin/UserController.php
+++ BackOffice_RateMyRent/src/Controller/Admin/UserController.php
@@ -5,6 +5,11 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\UserType;
 
 /**
  * @Route("/admin/user", name="admin_user_")
@@ -19,5 +24,32 @@ class UserController extends AbstractController
         return $this->render('admin/user/index.html.twig', [
             'users' => $repository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('admin/user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    //TODO edit
+
+
+    /**
+    * @Route("/{id}", name="delete", methods={"DELETE"})
+    */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_user_index');
     }
 }
