379e9b47225d62198157fbd30b038e1eab52a9a7
cambad <cam.dvhb@gmail.com>
2019-07-30 09:43:14 +0200

    Routing API ok

--- BackOffice_RateMyRent/src/Controller/Api/ApartmentController.php
+++ BackOffice_RateMyRent/src/Controller/Api/ApartmentController.php
@@ -5,13 +5,15 @@ namespace App\Controller\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Entity\Apartment;
 use App\Repository\ApartmentRepository;
 
+/**
+ * @Route("/api", name="api_")
+ */
 class ApartmentController extends AbstractController
 {
     /**
-     * @Route("/api/last_five_apartments", name="api_apartment")
+     * @Route("/five_apartments", name="five_apartments")
      */
     public function lastFiveApi(ApartmentRepository $repository): JsonResponse
     {
