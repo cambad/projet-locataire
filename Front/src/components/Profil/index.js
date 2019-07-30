83360fdaba3631b1507d31ffc8fe560ef5626d7d
MaximeRig <maxime.rigaud@ymail.com>
2019-07-30 09:07:16 +0200

    profil static

--- Front/src/components/Profil/index.js
+++ Front/src/components/Profil/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { FaUserAlt } from 'react-icons/fa';
 
 import './profil.scss';
 
 const Profil = () => (
   <main className="profil">
-    <h1 className="profil-title">Bienvenue sur votre profil * username *</h1>
-    <img className="profil-picture" src="#" alt="" />
-    <ul>
-      <li>Nom</li>
-      <li>Prénom</li>
-      <li>Adresse e-mail</li>
-    </ul>
+    <div className="profil-card">
+      <p className="profil-card-edit">Edite profile</p>
+      <FaUserAlt className="profil-card-picture" />
+      <ul className="profil-card-info">
+        <li className="profil-card-info-item">Nom</li>
+        <li className="profil-card-info-item">Prénom</li>
+        <li className="profil-card-info-item">Adresse e-mail</li>
+      </ul>
+    </div>
   </main>
 );
 
