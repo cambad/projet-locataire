1855e944d5ca53b75ed3cc8a05f2a39de1a0e036
MaximeRig <maxime.rigaud@ymail.com>
2019-08-01 18:51:18 +0200

    get five last apartments with ajax, display it or error message

--- Front/src/store/reducerMap.js
+++ Front/src/store/reducerMap.js
@@ -37,6 +37,7 @@ const reducer = (state = initialState, action = {}) => {
       return {
         ...state,
         redirectToMap: false,
+        address: '',
       };
     }
     case SET_ADDRESS_LAT_LNG: {
