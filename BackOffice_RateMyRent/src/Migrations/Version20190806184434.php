88f6ea77fb876aab1b9226ade5a8e3e8b32585a6
alexClmls <acalmels92@hotmail.fr>
2019-08-06 20:47:08 +0200

    new migration

--- BackOffice_RateMyRent/src/Migrations/Version20190806184434.php
+++ BackOffice_RateMyRent/src/Migrations/Version20190806184434.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190806093407 extends AbstractMigration
+final class Version20190806184434 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,10 @@ final class Version20190806093407 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE apartment (id INT AUTO_INCREMENT NOT NULL, address VARCHAR(256) NOT NULL, floor_number INT NOT NULL, location VARCHAR(128) NOT NULL, area INT NOT NULL, rooms INT NOT NULL, rental INT NOT NULL, lat DOUBLE PRECISION NOT NULL, lng DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, apartment_id INT DEFAULT NULL, title VARCHAR(256) NOT NULL, positive LONGTEXT NOT NULL, negative LONGTEXT NOT NULL, still_in TINYINT(1) NOT NULL, tenant TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_794381C6A76ED395 (user_id), INDEX IDX_794381C6176DFE85 (apartment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', surname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, age INT NOT NULL, UNIQUE INDEX UNIQ_8D93D64992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_8D93D649A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(64) NOT NULL, name VARCHAR(64) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_8D93D64992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_8D93D649A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, apartment_id INT DEFAULT NULL, title VARCHAR(256) NOT NULL, positive LONGTEXT NOT NULL, negative LONGTEXT NOT NULL, still_in TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_794381C6A76ED395 (user_id), INDEX IDX_794381C6176DFE85 (apartment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE apartment (id INT AUTO_INCREMENT NOT NULL, address VARCHAR(256) NOT NULL, floor_number INT NOT NULL, location VARCHAR(256) NOT NULL, area INT NOT NULL, rooms INT NOT NULL, rental INT NOT NULL, lat DOUBLE PRECISION NOT NULL, lng DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE marks (id INT AUTO_INCREMENT NOT NULL, review_id INT DEFAULT NULL, recommendation INT DEFAULT NULL, exterior INT DEFAULT NULL, interior INT DEFAULT NULL, contact INT DEFAULT NULL, accessibility INT DEFAULT NULL, apartment_environment INT DEFAULT NULL, traffic INT DEFAULT NULL, exterior_building INT DEFAULT NULL, building_environment INT DEFAULT NULL, insulation INT DEFAULT NULL, cleanliness INT DEFAULT NULL, brightness INT DEFAULT NULL, first_contact INT DEFAULT NULL, contact_quality INT DEFAULT NULL, INDEX IDX_3C6AFA533E2E969B (review_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6176DFE85 FOREIGN KEY (apartment_id) REFERENCES apartment (id)');
@@ -37,13 +37,13 @@ final class Version20190806093407 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6A76ED395');
-        $this->addSql('ALTER TABLE marks DROP FOREIGN KEY FK_3C6AFA533E2E969B');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6176DFE85');
-        $this->addSql('DROP TABLE role');
-        $this->addSql('DROP TABLE `user`');
-        $this->addSql('DROP TABLE review');
+        $this->addSql('ALTER TABLE marks DROP FOREIGN KEY FK_3C6AFA533E2E969B');
+        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6A76ED395');
         $this->addSql('DROP TABLE apartment');
+        $this->addSql('DROP TABLE review');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE role');
         $this->addSql('DROP TABLE marks');
     }
 }
