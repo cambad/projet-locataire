1f4580952271d9a6da9e3960700d5754943e8a09
alexClmls <acalmels92@hotmail.fr>
2019-08-06 20:32:40 +0200

    basic config FOSU

--- BackOffice_RateMyRent/src/Entity/User.php
+++ BackOffice_RateMyRent/src/Entity/User.php
@@ -3,39 +3,30 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
- * @ORM\Table(name="app_user")
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @UniqueEntity(fields={"username"}, message="Cet utilisateur existe déjà")
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
  */
-class User implements UserInterface, \Serializable
+class User extends BaseUser
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
      */
-    private $id;
+    protected $id;
 
     /**
-     * @ORM\Column(type="string", length=64)
+     * @ORM\Column(type="string")
      */
     private $surname;
 
     /**
-     * @ORM\Column(type="string", length=64)
+     * @ORM\Column(type="string")
      */
     private $name;
-
-    /**
-     * @ORM\Column(type="string", length=32, unique=true)
-     */
-    private $username;
 
     /**
      * @ORM\Column(type="integer")
@@ -43,236 +34,61 @@ class User implements UserInterface, \Serializable
     private $age;
 
     /**
-     * @ORM\Column(type="string", length=254, unique=true)
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=64)
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(name="is_active", type="boolean")
-     */
-    private $isActive;
-
-    /**
-     * @ORM\Column(type="string", length=256)
-     */
-    private $roleJsonFormat;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
-     */
-    private $role;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Review", mappedBy="user", cascade={"remove"})
-     */
-    private $reviews;
-
-    public function __construct()
-    {
-        $this->isActive = true;
-        $this->reviews = new ArrayCollection();
-        $this->roleJsonFormat = 'ROLE_MEMBER';
-    }
-
-    public function getRoles(): array 
-    {
-        $roles = []; 
-
-        if(!is_null($this->role)){
-
-            $roles[] = $this->role->getCode(); 
-
-        } else {
-            $roles[] = 'ROLE_ADMIN';
-        }
-
-        return $roles;
-    }
-
-    public function eraseCredentials()
-    {
-    }
-
-    /** @see \Serializable::serialize() */
-    public function serialize()
-    {
-        return serialize([
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt,
-        ]);
-    }
-
-    /** @see \Serializable::unserialize() */
-    public function unserialize($serialized)
-    {
-        list (
-            $this->id,
-            $this->username,
-            $this->password,
-            // see section on salt below
-            // $this->salt
-        ) = unserialize($serialized, ['allowed_classes' => false]);
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getSurname(): ?string
+     * Get the value of surname
+     */ 
+    public function getSurname()
     {
         return $this->surname;
     }
 
-    public function setSurname(string $surname): self
+    /**
+     * Set the value of surname
+     *
+     * @return  self
+     */ 
+    public function setSurname($surname)
     {
         $this->surname = $surname;
 
         return $this;
     }
 
-    public function getName(): ?string
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
     {
         return $this->name;
     }
 
-    public function setName(string $name): self
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
     {
         $this->name = $name;
 
         return $this;
     }
 
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function getSalt()
-    {
-        // you *may* need a real salt depending on your encoder
-        // see section on salt below
-        return null;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getAge(): ?int
+    /**
+     * Get the value of age
+     */ 
+    public function getAge()
     {
         return $this->age;
     }
 
-    public function setAge(int $age): self
+    /**
+     * Set the value of age
+     *
+     * @return  self
+     */ 
+    public function setAge($age)
     {
         $this->age = $age;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getIsActive(): ?bool
-    {
-        return $this->is_active;
-    }
-
-    public function setIsActive(bool $is_active): self
-    {
-        $this->is_active = $is_active;
-
-        return $this;
-    }
-
-    public function getRoleJsonFormat(): ?string
-    {   
-        $jsonRoles = json_decode($this->roleJsonFormat);
-
-        return $jsonRoles[0];
-    }
-
-    public function setRoleJsonFormat(string $roleJsonFormat = ''): self
-    {
-        $toJson = json_encode([$roleJsonFormat]);
-
-        $this->roleJsonFormat = $toJson;
-
-        return $this;
-    }
-
-    public function getRole(): ?Role
-    {
-        return $this->role;
-    }
-
-    public function setRole(?Role $role): self
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Review[]
-     */
-    public function getReviews(): Collection
-    {
-        return $this->reviews;
-    }
-
-    public function addReview(Review $review): self
-    {
-        if (!$this->reviews->contains($review)) {
-            $this->reviews[] = $review;
-            $review->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeReview(Review $review): self
-    {
-        if ($this->reviews->contains($review)) {
-            $this->reviews->removeElement($review);
-            // set the owning side to null (unless already changed)
-            if ($review->getUser() === $this) {
-                $review->setUser(null);
-            }
-        }
 
         return $this;
     }
