b843983d0a71b4441d3b1bd5c339c4c079fac1f1
MaximeRig <maxime.rigaud@ymail.com>
2019-08-07 14:42:01 +0200

    change design and data in reviews in landing page

--- Front/src/components/Review/index.js
+++ Front/src/components/Review/index.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from '@material-ui/lab/Rating';
 
 // locals imports
 import './review.scss';
 
-const Review = ({ address }) => (
-  <article className="review">
-    <div className="review-img" />
-    <div className="review-content">
-      <h3 className="review-address">{address}</h3>
-      <div className="review-description">
-        <div className="review-note">note : 4</div>
-        <div className="review-view">3 vues</div>
+const Review = ({
+  address,
+  contact,
+  exterior,
+  interior,
+  recommendation,
+  rental,
+  title,
+}) => {
+  const average = (contact + exterior + interior + recommendation) / 4;
+  return (
+    <article className="review">
+      <div className="review-img">
+        <p className="review-title">"{title}"</p>
       </div>
-    </div>
-  </article>
-);
+      <div className="review-content">
+        <h3 className="review-address">{address}</h3>
+        <div className="review-description">
+          <div className="review-note">loyer : {rental}€</div>
+          <div className="review-view">
+          Note moyenne
+            <Rating
+              value={average}
+              size="small"
+            />
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 Review.propTypes = {
   address: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  contact: PropTypes.number.isRequired,
+  exterior: PropTypes.number.isRequired,
+  interior: PropTypes.number.isRequired,
+  recommendation: PropTypes.number.isRequired,
+  rental: PropTypes.number.isRequired,
 };
 
 export default Review;
