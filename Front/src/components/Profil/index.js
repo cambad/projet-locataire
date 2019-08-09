dd645864040c3b34959b1d23c4c8b9e0d317182b
Methamorphe <methamorphe@gmail.com>
2019-08-09 10:45:08 +0200

    Starting dynamic profil

--- Front/src/components/Profil/index.js
+++ Front/src/components/Profil/index.js
@@ -3,19 +3,27 @@ import { FaUserAlt } from 'react-icons/fa';
 
 import './profil.scss';
 
-const Profil = () => (
-  <main className="profil">
-    <div className="profil-card">
-      <p className="profil-card-edit">Edite profile</p>
-      <FaUserAlt className="profil-card-picture" />
-      <ul className="profil-card-info">
-        <li className="profil-card-info-item">Nom</li>
-        <li className="profil-card-info-item">Prénom</li>
-        <li className="profil-card-info-item">Adresse e-mail</li>
-      </ul>
-    </div>
-  </main>
-);
+class Profil extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <main className="profil">
+        <div className="profil-card">
+          <p className="profil-card-edit">Editer profile</p>
+          <FaUserAlt className="profil-card-picture" />
+          <ul className="profil-card-info">
+            <li className="profil-card-info-item">Nom</li>
+            <li className="profil-card-info-item">Prénom</li>
+            <li className="profil-card-info-item">Adresse e-mail</li>
+          </ul>
+        </div>
+      </main>
+    );
+  }
+}
 
 
 export default Profil;
